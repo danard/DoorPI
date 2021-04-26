385247ef5ed86518999f336b9ede30011fb56dbe
DESKTOP-QSMDB5B\Danar <danardevol@gmail.com>
2021-04-27 00:36:43 +0200

    Nuevos métodos para Enviar peticion de Streaming al servidor con JSON

--- DoorPIApp/DoorPIApp/Models/TransmisionEntrada.cs
+++ DoorPIApp/DoorPIApp/Models/TransmisionEntrada.cs
@@ -1,18 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Threading.Tasks;
+using DoorPIApp.Services;
+using Newtonsoft.Json;
 
 namespace DoorPIApp.Models
 {
     class TransmisionEntrada
     {
+        class Identificador
+        {
+            public string Usuario { get; set; }
+            public string Contrasena { get; set; }
+        }
+        static Identificador LeerIdAjustes()
+        {
+            //Falta implementar => Leer ajustes
+            return new Identificador();
+        }
+
+        public static string JsonIdentificador()
+        {
+            Identificador id = LeerIdAjustes();
+            return JsonConvert.SerializeObject(id);
+        }
+
+        public static async Task<string> UrlTransmision()
+        {
+            string jsonId = JsonIdentificador();
+            return await Servidor.ObtenerEnlace(jsonId);
+        }
+
         public static string UrlTransmisionPrueba()
         {
             return "https://austchannel-live.akamaized.net/hls/live/2002736/austchannel-sport/master.m3u8";
-        }
-        public static string UrlTransmisionPrueba2()
-        {
-            return @"https://www.youtube.com/watch?v=b3HoTki90R8";
         }
     }
 }
---
+++ DoorPIApp/DoorPIApp/Services/Servidor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+
+namespace DoorPIApp.Services
+{
+    class Servidor
+    {
+        private static readonly HttpClient client = new HttpClient();
+        public const string ServidorEnlace = "http://nattech.fib.upc.edu:40330/streaming/";
+
+        public static async Task<string> ObtenerEnlace(string jsonIDserializado, string url = ServidorEnlace)
+        {
+            var json = new StringContent(jsonIDserializado);
+            var enlace = await client.PostAsync(url, json);
+            return await enlace.Content.ReadAsStringAsync();
+        }
+
+
+        //public HttpResponse Get(string Url = ServidorEnlace)
+        //{
+        //    var request = WebRequest.Create(Url);
+        //    request.ContentType = "application/json";
+        //    request.Method = "GET";
+        //    using (HttpWebResponse httpResponse = request.GetResponse() as HttpWebResponse)
+        //    {
+        //        return BuildResponse(httpResponse);
+        //    }
+        //}
+
+        //private static HttpResponse BuildResponse(HttpWebResponse httpResponse)
+        //{
+        //    using (StreamReader reader = new StreamReader(httpResponse.GetResponseStream()))
+        //    {
+        //        var content = reader.ReadToEnd();
+        //        var response = new HttpResponse();
+        //        response.Content = content;
+        //        response.HttpStatusCode = httpResponse.StatusCode;
+        //        return response;
+        //    }
+        //}
+
+        //public async Task<HttpResponse> GetAsync(string Url = ServidorEnlace)
+        //{
+        //    var request = WebRequest.Create(Url);
+        //    request.ContentType = "application/json";
+        //    request.Method = "GET";
+        //    using (HttpWebResponse httpResponse = await request.GetResponseAsync() as HttpWebResponse)
+        //    {
+        //        return BuildResponse(httpResponse);
+        //    }
+    }
+
+
+}
